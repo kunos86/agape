b710cd543634aa6fa0ea9ed9daa16e85bf1b2fa0
p.kunos@gmail.com <p.kunos@gmail.com@ad7c105b-6c3c-6461-4007-2a652fe3995b>
2012-07-17 22:18:38 +0000

    - funkcja wysyłania lekcji przez ucznia - strona niesprawdzonych lekcji dla nauczyciela

---
+++ trunk/agape-src/agape-ejb/src/main/java/pl/ksb/agape/domain/dao/LekcjaDoSprDAOLocal.java
@@ -0,0 +1,14 @@
+package pl.ksb.agape.domain.dao;
+
+import javax.ejb.Local;
+
+import org.hibernate.Criteria;
+
+import pl.ksb.agape.domain.model.view.LekcjaDoSpr;
+
+@Local
+public interface LekcjaDoSprDAOLocal {
+	LekcjaDoSpr getById(Long id);
+
+	Criteria wszystkieLekcjeDoSprByIdNauczycielCriteria(Long idNauczciela);
+}
