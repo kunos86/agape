d720b8fc1d971c241ac7ba2566bae47b1297fef6
p.kunos <p.kunos@gmail.com>
2014-03-17 10:45:13 +0000

    poprawa błędu zaczytywania listy dostępnych lekcji

--- agape-src/agape/src/main/java/pl/ksb/agape/view/bean/student/LessonListBrowser.java
+++ agape-src/agape/src/main/java/pl/ksb/agape/view/bean/student/LessonListBrowser.java
@@ -1,7 +1,9 @@
 package pl.ksb.agape.view.bean.student;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -10,7 +12,10 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import pl.ksb.agape.domain.dao.CourseDAOBean;
+import pl.ksb.agape.domain.dao.EducationStateDAOBean;
 import pl.ksb.agape.domain.model.Course;
+import pl.ksb.agape.domain.model.EducationState;
+import pl.ksb.agape.domain.model.Lesson;
 import pl.ksb.agape.util.Encoder;
 import pl.ksb.agape.view.bean.SessionLoggedUser;
 
@@ -26,18 +31,11 @@ public class LessonListBrowser implements Serializable {
 	private CourseDAOBean courseDAOBean;
 	Collection<Course> courses;
 
+	@EJB
+	private EducationStateDAOBean educationStateDAOBean;
+
 	@ManagedProperty(value = "#{sessionLoggedUser}")
 	private SessionLoggedUser sessionLoggedUser;
-
-	public void setSessionLoggedUser(SessionLoggedUser sessionLoggedUser) {
-		this.sessionLoggedUser = sessionLoggedUser;
-	}
-
-	@PostConstruct
-	public void init() {
-		courses = courseDAOBean.getCoursesListForStudent(sessionLoggedUser
-				.getUser().getId());
-	}
 
 	public Collection<Course> getCourses() {
 		return courses;
@@ -50,6 +48,28 @@ public class LessonListBrowser implements Serializable {
 	public String goToLesson(Long idLesson) {
 		return "/pages/lekcja.xhtml?faces-redirect=true&idLekcja="
 				+ Encoder.encode(idLesson);
+	}
+
+	@PostConstruct
+	public void init() {
+		courses = courseDAOBean.getCoursesListForStudent(sessionLoggedUser
+				.getUser().getId());
+		// obejscie problemu z zaczytaniem przez hibernete
+		for (Course c : courses) {
+			for (Lesson l : c.getLessons()) {
+				EducationState es = educationStateDAOBean
+						.getByIdLessonIdStudent(l.getId(), sessionLoggedUser
+								.getUser().getId());
+				List<EducationState> list = new ArrayList<EducationState>();
+				list.add(es);
+				l.setEducationStates(list);
+			}
+		}
+
+	}
+
+	public void setSessionLoggedUser(SessionLoggedUser sessionLoggedUser) {
+		this.sessionLoggedUser = sessionLoggedUser;
 	}
 
 }
