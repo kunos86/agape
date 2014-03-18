5f25dc6e45b4dee99678bda9157b106afa6b74a6
p.kunos <p.kunos@gmail.com>
2014-03-18 11:38:02 +0000

    funkcja zmiany hasła

---
+++ agape-src/agape/src/main/java/pl/ksb/agape/view/bean/ChangePasswordBrowser.java
@@ -0,0 +1,78 @@
+package pl.ksb.agape.view.bean;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import pl.ksb.agape.domain.dao.UserDAOBean;
+import pl.ksb.agape.util.CryptoTools;
+
+@ManagedBean
+@RequestScoped
+public class ChangePasswordBrowser {
+
+	private String confirmPassword;
+
+	private String currentPassword;
+
+	private String newPassword;
+	@ManagedProperty(value = "#{sessionLoggedUser}")
+	private SessionLoggedUser sessionLoggedUser;
+
+	@EJB
+	private UserDAOBean userDAOBean;
+
+	public void changePassword() {
+
+		if (!userDAOBean.authenticat(sessionLoggedUser.getUser().getEmail(),
+				CryptoTools.getInstance().passwordHash(currentPassword))) {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN,
+							"Aktualne hasło jest niepoprawne!",
+							"Aktualne hasło jest niepoprawne!"));
+			return;
+		}
+		userDAOBean.changePassword(sessionLoggedUser.getUser().getId(),
+				CryptoTools.getInstance().passwordHash(newPassword));
+
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Twoje hasło zostało zmienione!",
+						"Twoje hasło zostało zmienione!"));
+
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public String getCurrentPassword() {
+		return currentPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public void setCurrentPassword(String currentPassword) {
+		this.currentPassword = currentPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public void setSessionLoggedUser(SessionLoggedUser sessionLoggedUser) {
+		this.sessionLoggedUser = sessionLoggedUser;
+	}
+
+}
